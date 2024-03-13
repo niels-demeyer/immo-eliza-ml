65f1b4a0745f7fbe01794824dba9cfd9d3829f89
Niels Demeyer <niels.demeyer99@gmail.com>
2024-03-13 10:49:42 +0100

    added gitignore

--- ImmoClass_file.py
+++ ImmoClass_file.py
@@ -17,14 +17,9 @@ class ImmoClass:
     # Load the houses data
     def load_houses_data_pandas(self):
         script_dir = os.path.dirname(__file__)
-        rel_path = "../data/cleaned/properties.csv"
+        rel_path = "data\raw\properties.csv"
         file_path = os.path.join(script_dir, rel_path)
         self.houses_data = pd.read_csv(file_path)
-        self.houses_data["city"] = (
-            self.houses_data["city"].str.lower().str.strip()
-        )  # convert city names to lowercase and remove whitespaces
-        self.count_houses = self.houses_data["city"].value_counts().reset_index()
-        self.count_houses.columns = ["city", "count"]
 
     def streamlit_app(self):
         st.set_page_config(page_title="Belgium Real Estate Analysis", layout="wide")
---
+++ train.py
@@ -0,0 +1,6 @@
+from ImmoClass_file import ImmoClass
+
+# Create an instance of the StreamLitClass and load the data
+immo = ImmoClass()
+
+print(immo.houses_data.head())
