845e6426bd2007cc9889cea1e4ab18bcf35dcfe3
Niels Demeyer <niels.demeyer99@gmail.com>
2024-03-13 10:37:44 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,8 @@
+from ImmoClass_file import ImmoClass
+
+# Create an instance of the StreamLitClass and load the data
+streamlit_app = ImmoClass()
+
+
+# Create the Streamlit app
+streamlit_app.streamlit_app()
