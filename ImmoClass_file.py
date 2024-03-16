df09f056df6e1c4c6128110bd75b3a5c6439d9d8
Niels Demeyer <niels.demeyer99@gmail.com>
2024-03-16 15:35:39 +0100

    changed to auto ml

--- ImmoClass_file.py
+++ ImmoClass_file.py
@@ -1,15 +1,12 @@
-import streamlit as st
-import os
-from plotly import express as px
-from plotly import graph_objs as go
-import pandas as pd
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
+import autosklearn.classification
+import os
+import pandas as pd
 
 
 class ImmoClass:
@@ -19,7 +16,7 @@ class ImmoClass:
         self.X_test = None
         self.y_train = None
         self.y_test = None
-        self.model = None
+        self.model = autosklearn.classification.AutoSklearnClassifier()
         self.preprocessor = None
 
         # load the data
@@ -27,16 +24,16 @@ class ImmoClass:
 
         # clean the data
         self.clean_data()
-        
+
         self.split_data()
 
-        # # preprocess the data
-        # self.preprocessor = self.create_preprocessor(X_train)
-        # X_train = self.preprocess_data(X_train)
-        # X_test = self.preprocess_data(X_test)
+        # preprocess the data
+        self.preprocessor = self.create_preprocessor(self.X_train)
+        self.X_train = self.preprocess_data(self.X_train)
+        self.X_test = self.preprocess_data(self.X_test)
 
-        # # train the model
-        # self.train_model_linear(X_train, X_test, y_train, y_test)
+        # train the model
+        self.train_model()
 
     # Load the houses data
     def load_houses_data_pandas(self):
@@ -60,51 +57,35 @@ class ImmoClass:
             "primary_energy_consumption_sqm",
             "cadastral_income",
         ]
-        cols_to_convert = [
-            col for col in cols_to_convert if col in self.houses_data.columns
-        ]
         self.houses_data[cols_to_convert] = self.houses_data[cols_to_convert].apply(
             pd.to_numeric, errors="coerce"
         )
-        self.houses_data[cols_to_convert] = self.houses_data[cols_to_convert].apply(
-            pd.to_numeric, errors="coerce"
-        )
-
-        # Handle missing values
-        #drop rows with missing price
-        self.houses_data.dropna(subset=["price"], inplace=True)
-
-        # Remove duplicates
-        self.houses_data.drop_duplicates(inplace=True)
 
     def split_data(self):
-        # Split the data into training and testing sets
-        X = self.houses_data.drop("price", axis=1)
-        y = self.houses_data["price"]
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
-            X, y, test_size=0.2, random_state=42
+            self.houses_data.drop("price", axis=1),
+            self.houses_data["price"],
+            test_size=0.2,
+            random_state=42,
         )
-        
-    def create_preprocessor(self, data):
-        # Define preprocessing for numeric columns (scale them)
-        numeric_features = data.select_dtypes(include=["int64", "float64"]).columns
+
+    def create_preprocessor(self, X):
+        numeric_features = X.select_dtypes(include=["int64", "float64"]).columns
         numeric_transformer = Pipeline(
             steps=[
-                ("imputer", SimpleImputer(strategy="mean")),
+                ("imputer", SimpleImputer(strategy="median")),
                 ("scaler", StandardScaler()),
             ]
         )
 
-        # Define preprocessing for categorical features (one-hot encode them)
-        categorical_features = data.select_dtypes(include=["object"]).columns
+        categorical_features = X.select_dtypes(include=["object"]).columns
         categorical_transformer = Pipeline(
             steps=[
-                ("imputer", SimpleImputer(strategy="constant", fill_value="MISSING")),
+                ("imputer", SimpleImputer(strategy="constant", fill_value="missing")),
                 ("onehot", OneHotEncoder(handle_unknown="ignore")),
             ]
         )
 
-        # Combine preprocessing steps
         preprocessor = ColumnTransformer(
             transformers=[
                 ("num", numeric_transformer, numeric_features),
@@ -112,44 +93,13 @@ class ImmoClass:
             ]
         )
 
-        # Fit the preprocessor on the data
-        preprocessor.fit(data)
-
         return preprocessor
 
-    def preprocess_data(self, data):
-        # Apply transformations to the data
-        return self.preprocessor.transform(data)
+    def preprocess_data(self, X):
+        return self.preprocessor.fit_transform(X)
 
-    def train_model_linear(self, X_train, X_test, y_train, y_test):
-        # Define the model
-        model = LinearRegression()
+    def train_model(self):
+        self.model.fit(self.X_train, self.y_train)
 
-        # Train the model
-        model.fit(X_train, y_train)
-
-        # Save the trained model
-        self.model = model
-
-        # Print the model's coefficients and intercept
-        print(f"Model coefficients: {model.coef_}")
-        print(f"Model intercept: {model.intercept_}")
-
-        # Predict on the test set
-        y_pred = model.predict(X_test)
-
-        # Calculate and print the R^2 score
-        r2 = r2_score(y_test, y_pred)
-        print(f"R^2 score: {r2}")
-
-        # Calculate and print the mean squared error
-        mse = mean_squared_error(y_test, y_pred)
-        print(f"Mean squared error: {mse}")
-
-    def streamlit_app(self):
-        st.set_page_config(page_title="Belgium Real Estate Analysis", layout="wide")
-
-        st.title("Belgium Real Estate Analysis")
-        st.markdown(
-            "This is a simple web app that shows the average price of houses per municipality in Belgium"
-        )
+    def predict(self, X):
+        return self.model.predict(X)
