14b8c6e2d205bfdd1c766472df419137e426111d
Niels Demeyer <niels.demeyer99@gmail.com>
2024-03-20 12:05:17 +0100

    ran for random_forest

--- random_forest.py
+++ random_forest.py
@@ -5,6 +5,8 @@ if __name__ == "__main__":
     houses = ImmoClass("HOUSE")
     houses.train_model_random_forest()
     houses.print_model_results("random_forest")
+    houses.save_model_results()
     apartments = ImmoClass("APARTMENT")
     apartments.train_model_random_forest()
     apartments.print_model_results("random_forest")
+    apartments.save_model_results()
