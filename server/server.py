4626bf31d6138e1583036889f766e78df1ca9f89
nelzon <niels.demeyer99@gmail.com>
2024-03-25 10:45:23 +0100

    Fix bug in login functionality

---
+++ server/server.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, jsonify
+from training.ImmoClass_file import ImmoClass
+
+app = Flask(__name__)
+
+@app.route('/predict/house', methods=['POST'])
+def predict_house():
+    data = request.get_json()
+    immo_house = ImmoClass("HOUSE")
+    prediction = immo_house.predict_house(data)
+    return jsonify({'predicted_price': prediction})
+
+@app.route('/predict/apartment', methods=['POST'])
+def predict_apartment():
+    data = request.get_json()
+    immo_apartment = ImmoClass("APARTMENT")
+    prediction = immo_apartment.predict_apartment(data)
+    return jsonify({'predicted_price': prediction})
+
+if __name__ == "__main__":
+    app.run(debug=True)
