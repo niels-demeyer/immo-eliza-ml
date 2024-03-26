cd81d38f4429fc08d6cc61717f5f28de38e0d89a
niels demeyer <niels.demeyer99@gmail.com>
2024-03-26 15:21:00 +0100

    init the project

--- client/immo/tailwind.config.js
+++ client/immo/tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: [
+          "Inter",
+          "system-ui",
+          "Avenir",
+          "Helvetica",
+          "Arial",
+          "sans-serif",
+        ],
+      },
+      colors: {
+        light: "rgba(255, 255, 255, 0.87)",
+        dark: "#242424",
+      },
+    },
   },
   plugins: [],
 };
