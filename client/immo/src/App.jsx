772a8589f77d7b0019f2a559acaa2cd8cc049acd
niels demeyer <niels.demeyer99@gmail.com>
2024-03-26 17:47:20 +0100

    adding chadcn

--- client/immo/src/App.jsx
+++ client/immo/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import PostRequestComponent from "./components/PostRequestComponent"; // Import PostRequestComponent
+import Button from "./components/ui/button"; // Change this line
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <PostRequestComponent />
+      <Button>Click me</Button>
     </div>
   );
 }
