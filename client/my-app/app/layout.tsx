f893a28f19b8e1c5c77918ccb4ef0bdb9eb8c8d9
niels demeyer <niels.demeyer99@gmail.com>
2024-03-26 13:54:52 +0100

    init the shadcn ui

---
+++ client/my-app/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Price Prediction",
+  description:
+    "This is a price prediction tool which uses machine learning to predict the price of real estate.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
